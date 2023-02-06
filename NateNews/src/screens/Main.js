5875af0467ad00f7b365b337f79e7f394495bb7d
frica12 <fricaa@naver.com>
2023-02-06 20:59:57 +0900

    update

--- NateNews/src/screens/Main.js
+++ NateNews/src/screens/Main.js
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components/native";
-import { Button } from "react-native";
+import React, { useContext, useState, useRef } from "react";
+import styled, { ThemeContext } from "styled-components/native";
+import { Button, Image, Input } from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Container = styled.View`
@@ -13,17 +13,47 @@ const Container = styled.View`
   padding-bottom: ${({ insets: { bottom } }) => bottom}px;
 `;
 
-const StyledText = styled.Text`
-  font-size: 30px;
-  color: #111111;
-`;
+const LOGO =
+  "https://firebasestorage.googleapis.com/v0/b/natenews-64341.appspot.com/o/nateicon.png?alt=media";
 
 const Main = ({ navigation }) => {
   const insets = useSafeAreaInsets();
+  const theme = useContext(ThemeContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const refPassword = useRef(null);
+
+  const _handleSignBtnPress = () => {
+    console.log("News");
+  };
+
   return (
     <Container insets={insets}>
-      <StyledText>Main</StyledText>
-      <Button title="News" onPress={() => navigation.navigate("News")} />
+      <Image url={LOGO} />
+      <Input
+        ref={refPassword}
+        label="Email"
+        placeholder="Email"
+        returnKeyType="next"
+        value={email}
+        onChangeText={setEmail}
+        onSubmitEditing={() => refPassword.current.focus()}
+      />
+      <Input
+        label="Password"
+        placeholder="Password"
+        returnKeyType="done"
+        value={password}
+        onChangeText={setPassword}
+        isPassword={true}
+      />
+      <Button
+        title="News"
+        onPress={() => navigation.navigate("News")}
+        containerStyle={{}}
+        textStyle={{ color: theme.white, fontSize: 25 }}
+      />
     </Container>
   );
 };
--- NateNews/src/theme.js
+++ NateNews/src/theme.js
@@ -1,10 +1,26 @@
 const Colors = {
   white: "#ffffff",
   black: "#111111",
-  main: "#3679fe",
+  main: "#FA5858",
+  grey_0: "#d5d5d5",
+  grey_1: "#a6a6a6",
 };
 
 export const theme = {
   background: Colors.white,
-  text: Colors.text,
+  text: Colors.black,
+
+  // Button
+  btnBackground: Colors.main,
+  btnTitle: Colors.white,
+  btnTextLink: Colors.main,
+
+  // Image
+  imgBackground: Colors.grey,
+
+  // Input
+  inputBackground: Colors.white,
+  inputLabel: Colors.grey_1,
+  inputPlaceholder: Colors.grey_1,
+  inputBorder: Colors.grey_1,
 };
